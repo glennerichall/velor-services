924e33d8863229293afa7df689df5b52cfa0f262
Glenn Hall <glennerichall@hotmail.com>
2024-11-30 14:30:11 -0500

    fixed getLogger

--- injection/services.mjs
+++ injection/services.mjs
@@ -4,8 +4,8 @@ import {
     s_logger
 } from "./serviceKeys.mjs";
 import {isServiceAware} from "./ServicesContext.mjs";
-import {noOp} from "velor-utils/utils/functional.mjs";
 import winston from "winston";
+import {noOpLogger} from "velor-utils/utils/noOpLogger.mjs";
 
 export function getEnvNameResolver(serviceAware) {
     return getProvider(serviceAware)[s_envNameResolver]();
@@ -18,26 +18,27 @@ export function setLogger(holder, logger) {
 }
 
 export function getLogger(servicesAware) {
-    if (isServiceAware(servicesAware)) {
-        return getProvider(servicesAware)[s_logger]();
-    } else if (servicesAware[loggerSymbol] !== undefined) {
-        return servicesAware[loggerSymbol];
-    } else if (process.env.ZUPFE_LOG_LEVEL) {
-        return winston.createLogger({
-            level: process.env.LOG_LEVEL,
-            transports: [new winston.transports.Console()],
-            format: winston.format.simple(),
-        })
-    } else {
-        return {
-            trace: noOp,
-            silly: noOp,
-            debug: noOp,
-            warn: noOp,
-            info: noOp,
-            error: noOp,
-            fatal: noOp,
-            log: noOp,
+    let logger;
+    if (servicesAware[loggerSymbol] !== undefined) {
+        logger = servicesAware[loggerSymbol];
+    } else if (isServiceAware(servicesAware)) {
+        try {
+            logger = getProvider(servicesAware)[s_logger]();
+        } catch (e) {
         }
     }
+
+    if (!logger) {
+        if (process.env.LOG_LEVEL) {
+            logger = winston.createLogger({
+                level: process.env.LOG_LEVEL,
+                transports: [new winston.transports.Console()],
+                format: winston.format.simple(),
+            })
+        } else {
+            logger = noOpLogger
+        }
+    }
+
+    return logger;
 }
